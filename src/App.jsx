b8ed13800768c60811ac9c5b2ff85526c295999f
007tom <zhyd007@gmail.com>
2020-06-05 23:08:42 +0800

    Configuring React

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React, {Component} from 'react';
+import './App.css';
+
+import { Navbar, NavbarBrand } from 'reactstrap';
+
+class App extends Component {
+    render() {
+        return (
+            <div className={"App"}>
+                <Navbar dark color="primary">
+                    <div className={"container"}>
+                        <NavbarBrand href="/">
+                            Ristorante Con Fusion
+                        </NavbarBrand>
+                    </div>
+                </Navbar>
+            </div>
+        );
+    }
+}
+
+export default App;
