511133e790bd27f0618a36086067dd1edc8d0d06
007tom <zhyd007@gmail.com>
2020-06-09 16:24:47 +0800

    refactor: 'Redux Thunk'

--- src/components/DishDetail.js
+++ src/components/DishDetail.js
@@ -3,6 +3,7 @@ import {Card, CardBody, CardImg, CardText, CardTitle, Breadcrumb, BreadcrumbItem
     Button, Modal, ModalHeader, ModalBody, Label} from "reactstrap";
 import { Control, LocalForm, Errors } from "react-redux-form";
 import { Link } from "react-router-dom";
+import { Loading } from "./Loading";
 
 const maxLength = (len) => (val) => !(val) || val.length <= len;
 const minLength = (len) => (val) => (val) && val.length >= len;
@@ -174,6 +175,23 @@ class CommentForm extends React.Component {
 }
 
 const DishDetail = (props) => {
+    if (props.isLoading) {
+        return (
+            <div className="container">
+                <div className="row">
+                    <Loading />
+                </div>
+            </div>
+        );
+    } else if (props.errMess) {
+        return (
+            <div className="container">
+                <div className="row">
+                    <h4>{props.errMess}</h4>
+                </div>
+            </div>
+        );
+    } else if (props.dish != null) {
         return (
             <div>
                 <div className="container">
@@ -186,11 +204,11 @@ const DishDetail = (props) => {
                         </Breadcrumb>
                         <div className="col-12">
                             <h3>{props.dish.name}</h3>
-                            <hr />
+                            <hr/>
                         </div>
                     </div>
                     <div className="row">
-                        <RenderDish dish={props.dish} />
+                        <RenderDish dish={props.dish}/>
                         <RenderComments
                             comments={props.comments}
                             addComment={props.addComment}
@@ -201,5 +219,6 @@ const DishDetail = (props) => {
             </div>
         );
     }
+}
 
 export default DishDetail;
