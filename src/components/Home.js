ead823f17359eb6eff80a9cb0636b74c1e5eafb2
007tom <zhyd007@gmail.com>
2020-06-16 17:32:38 +0800

    style: modified 'baseUrl' to server-side

--- src/components/Home.js
+++ src/components/Home.js
@@ -22,22 +22,17 @@ function RenderCard({item, isLoading, errMess}) {
         );
     } else {
         return (
-            <FadeTransform
-                in
-                transformProps={{
-                    exitTransform: 'scale(0.5) translateY(-50%)'
-                }}
-            >
+            <FadeTransform in transformProps={{ exitTransform: 'scale(0.5) translateY(-50%)' }}>
                 <Card>
-                <CardImg src={baseUrl + item.image} alt={item.name}/>
-                <CardBody>
-                    <CardTitle>
-                        {item.name}
-                    </CardTitle>
-                    {item.designation ? <CardSubtitle>{item.designation}</CardSubtitle> : null}
-                    <CardText>{item.description}</CardText>
-                </CardBody>
-            </Card>
+                    <CardImg src={baseUrl + item.image} alt={item.name}/>
+                    <CardBody>
+                        <CardTitle>
+                            {item.name}
+                        </CardTitle>
+                        {item.designation ? <CardSubtitle>{item.designation}</CardSubtitle> : null}
+                        <CardText>{item.description}</CardText>
+                    </CardBody>
+                </Card>
             </FadeTransform>
         );
     }
