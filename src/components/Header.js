7487103bcc092f352c5e9eeff68a3adff9fdbd1e
007tom <zhyd007@gmail.com>
2020-06-19 11:35:12 +0800

    fix: logo loading

--- src/components/Header.js
+++ src/components/Header.js
@@ -74,7 +74,7 @@ class Header extends Component {
                     <div className="container">
                         <NavbarToggler onClick={this.toggleNav} />
                         <NavbarBrand className="mr-auto" href="/">
-                            <img src='assets/images/logo.png' height="30" width="41" alt='Ristorante Con Fusion' />
+                            <img src='logo.png' height="30" width="41" alt='Ristorante Con Fusion' />
                         </NavbarBrand>
                         <Collapse isOpen={this.state.isNavOpen} navbar>
                             <Nav navbar>
