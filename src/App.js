613214d6169f49263bb0b67be239cf8539ec5356
lu <10154508169@stu.ecnu.edu.cn>
2022-03-15 00:36:17 +0000

    test action.

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. (develop-action-branch)
+          Edit <code>src/App.js</code> and save to reload. (test-action)
         </p>
         <a
           className="App-link"
